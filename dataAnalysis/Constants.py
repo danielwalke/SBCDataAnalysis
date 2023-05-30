fd2ced9abe988305393755da5b742a7d53920572
danielwalke <daniel.walke@t-online.de>
2023-05-30 11:16:19 +0200

    minor change in analysis

--- dataAnalysis/Constants.py
+++ dataAnalysis/Constants.py
@@ -15,7 +15,7 @@ REV_EDGE_TYPE = "rev_HAS"
 VARIATION_DF = "variation.csv"
 VARIATION_SMALL_DF = "variation_small.csv"
 
-STEPS = 14 # for HetGNN 14 to reduce computational complexity; otherwise 20
+STEPS = 20 # for HetGNN 14 to reduce computational complexity; otherwise 20
 
 FEATURES = [AGE_COLUMN_NAME, SEX_CATEGORY_COLUMN_NAME, HGB_COLUMN_NAME, WBC_COLUMN_NAME, RBC_COLUMN_NAME, MCV_COLUMN_NAME, PLT_COLUMN_NAME]
 
